a3553d88a28dbd75faa0fd890eb09f22168aac4b
RedGnom <147233214+RedGnom@users.noreply.github.com>
2023-10-11 11:25:56 +0500

    Обновление функции вывода массива

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -11,7 +11,7 @@ void fillarr(int* const arr, int const size) {
 }
 void showarr(int* const arr, int const size) {
 	for (int i = 0; i < size; i++) {
-		cout << arr[i] << endl;
+		cout << arr[i] << "\t";
 	}
 	
 
