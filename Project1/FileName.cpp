f08693b31c1cf8bb893dd76bc8cf52cd985b35be
RedGnom <arsik172004@mail.ru>
2023-10-23 17:49:33 +0500

    добавление фото

---
+++ Project1/FileName.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string message{ "Hello METANIT.COM!" };
+    std::cout << "Message: " << message << std::endl; // Message: Hello METANIT.COM!
+}
